e9999473683c597aed79de63adf3ea73337f3630
Nathan D <123486140+diffeyn@users.noreply.github.com>
2026-02-17 10:33:55 -0600

    feat: add start-maximized option to Selenium WebDriver setup

--- src/mls/utils/scraping/selenium_helpers.py
+++ src/mls/utils/scraping/selenium_helpers.py
@@ -18,6 +18,7 @@ def set_up_driver():
     options.add_argument("--headless=new")
     options.add_argument("--disable-gpu")
     options.add_argument("--no-sandbox")
+    options.add_argument("--start-maximized")
 
     options.add_argument("--disable-blink-features=AutomationControlled")
     options.add_experimental_option("excludeSwitches", ["enable-automation"])
