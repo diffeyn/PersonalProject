41c647b72258d01ac185b360d12735f681f5f052
Nathan D <123486140+diffeyn@users.noreply.github.com>
2026-02-17 10:49:31 -0600

    refactor: update output paths for cleaned match and SoFIFA data

--- src/mls/cleaning/clean.py
+++ src/mls/cleaning/clean.py
@@ -10,12 +10,11 @@ def clean_data():    ## clean match data
     
     cleaned_match_team_stats, cleaned_match_player_stats, cleaned_feed = clean_matches.clean_matches()
     
-    cleaned_match_team_stats.to_csv(f"{outdir}/matches/cleaned_match_team_stats.csv", index=False)
-    cleaned_match_player_stats.to_csv(f"{outdir}/matches/cleaned_match_player_stats.csv", index=False)
-    cleaned_feed.to_csv(f"{outdir}/matches/cleaned_match_feed.csv", index=False)  
+    cleaned_match_team_stats.to_csv(f"{outdir}/cleaned_matches/cleaned_match_team_stats.csv", index=False)
+    cleaned_match_player_stats.to_csv(f"{outdir}/cleaned_matches/cleaned_match_player_stats.csv", index=False)
+    cleaned_feed.to_csv(f"{outdir}/cleaned_matches/cleaned_match_feed.csv", index=False)  
     
     ## clean SoFIFA data
     cleaned_players, cleaned_team = clean_sofifa.clean_sofifa()
-    cleaned_team.to_csv(f"{outdir}/sofifa/cleaned_team_stats.csv", index=False)
-    cleaned_players.to_csv(f"{outdir}/sofifa/cleaned_player_stats.csv", index=False)    
-    
+    cleaned_team.to_csv(f"{outdir}/cleaned_sofifa/cleaned_team_stats.csv", index=False)
+    cleaned_players.to_csv(f"{outdir}/cleaned_sofifa/cleaned_player_stats.csv", index=False)
