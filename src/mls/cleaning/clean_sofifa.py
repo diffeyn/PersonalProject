5b4516fc6c506e5f4a779062fb48a2445ae14923
Nathan D <123486140+diffeyn@users.noreply.github.com>
2026-02-17 10:57:15 -0600

    refactor: update file paths for player and team stats in clean_sofifa function

--- src/mls/cleaning/clean_sofifa.py
+++ src/mls/cleaning/clean_sofifa.py
@@ -5,8 +5,8 @@ from mls.cleaning.sofifa import clean_player_stats, clean_team_stats
 
 def clean_sofifa():
     
-    player_stats = pd.read_csv('./tests/data/interim/sofifa/sofifa_player_stats.csv')    
-    team_stats = pd.read_csv('./tests/data/interim/sofifa/sofifa_team_stats.csv')
+    player_stats = pd.read_csv('data/raw/sofifa/sofifa_player_stats.csv')    
+    team_stats = pd.read_csv('data/raw/sofifa/sofifa_team_stats.csv')
     cleaned_player_stats = clean_player_stats.clean_player_stats(player_stats)
     cleaned_team_stats = clean_team_stats.clean_team_stats(team_stats)
     
