77235c7653ec7f3ff72f6eb340c19a5f7d251feb
Nathan D <123486140+diffeyn@users.noreply.github.com>
2026-02-23 17:52:26 -0600

    Fix column renaming from "id" to "ID" in players_finance, players_general, players_stats, and team_roster modules

--- src/mls/database/sofifa/players_stats.py
+++ src/mls/database/sofifa/players_stats.py
@@ -2,6 +2,6 @@ import pandas as pd
 
 def get_player_stats(df):
     df = df.copy()
-    df.rename(columns={"id": "player_id"}, inplace=True)
+    df.rename(columns={"ID": "player_id"}, inplace=True)
     df = df.drop(columns=['name', 'height_cm', 'weight_kg', 'team_name', 'contract_start', 'contract_end', 'position', 'foot'])
     return df
